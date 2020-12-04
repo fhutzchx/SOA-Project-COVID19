4b14ac2a769b1329cac3f4c1761929f585ae7da6
Ittiwat Whangdee <62237690+fhutzchx@users.noreply.github.com>
2020-12-04 20:47:06 +0700

    Add Ejs

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const Db = require('../Db/database');
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const result = await Db.getAllCountry();
+  const confirmed = await Db.getAllConfirmed();
+  const recovered = await Db.getAllRecovered();
+  const death = await Db.getAllDeath();
+
+  let objectCountry = [];
+  for (const key in result.rows) {
+    objectCountry[key] = {
+      state: result.rows[key].state,
+      country: result.rows[key].country,
+      confirmed: confirmed.rows[key].confirmed,
+      recovered: recovered.rows[key].confirmed,
+      death: death.rows[key].confirmed,
+    }
+  }
+  res.render('index', { countrys: objectCountry });
+});
+
+router.get('/map', async function(req,res,next) {
+  res.render('map');
+
+});
+
+module.exports = router;
