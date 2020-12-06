0deefe47bab813571551695dbc47b716f4c7f1f8
fhutzchx <fahut.ittiwat@gmail.com>
2020-12-07 02:09:49 +0700

    add garph

--- Db/database.js
+++ Db/database.js
@@ -62,10 +62,221 @@ const getLatLong = async () => {
     }
 }
 
+const getAllConfirmedByCountry = async (Country) => {
+    const sql = `SELECT * from covid19_confirmed_csv where "Country/Region" = '${Country}' `;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllRecoveredByCountry = async (Country) => {
+    const sql = `SELECT * from covid19_recovered_csv where "Country/Region" = '${Country}' `;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllDeathsByCountry = async (Country) => {
+    const sql = `SELECT * from covid19_death_csv where "Country/Region" = '${Country}' `;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllConfirmedByState = async (State) => {
+    const sql = `SELECT * from covid19_confirmed_csv where "Province/State" = '${State}'`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllRecoveredByState = async (State) => {
+    const sql = `SELECT * from covid19_recovered_csv where "Province/State" = '${State}'`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getAllDeathsByState = async (State) => {
+    const sql = `SELECT * from covid19_death_csv where "Province/State" = '${State}'`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getLatLongCountry = async () => {
+    const sql = `select "Province/State" as State, "Country/Region" as Country, lat, long from covid19_confirmed_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+const getLastUpdateRecovered = async () => {
+    const sql = `SELECT "3/22/20" as Recovered from covid19_recovered_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+const getLastUpdateDeaths = async () => {
+    const sql = `SELECT "3/22/20" as Deaths from covid19_death_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+
+///////////////////// getChart /////////////////////
+
+const getTotalConfirmed = async () => {
+    const sql = `SELECT sum("3/22/20") as Confirmed from covid19_confirmed_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getTotalRecovered = async () => {
+    const sql = `SELECT sum("3/22/20") as Recovered from covid19_recovered_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getTotalDeaths = async () => {
+    const sql = `SELECT sum("3/22/20") as Deaths from covid19_death_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getLastWeekConfirmed = async () => {
+    const sql = `select
+    sum("3/16/20") as Day1,
+    sum("3/17/20") as Day2,
+    sum("3/18/20") as Day3,
+    sum("3/19/20") as Day4,
+    sum("3/20/20") as Day5,
+    sum("3/21/20") as Day6,
+    sum("3/22/20") as Day7
+    from covid19_confirmed_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getLastWeekRecovered = async () => {
+    const sql = `select
+    sum("3/16/20") as Day1,
+    sum("3/17/20") as Day2,
+    sum("3/18/20") as Day3,
+    sum("3/19/20") as Day4,
+    sum("3/20/20") as Day5,
+    sum("3/21/20") as Day6,
+    sum("3/22/20") as Day7
+    from covid19_recovered_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+const getLastWeekDeaths = async () => {
+    const sql = `select
+    sum("3/16/20") as Day1,
+    sum("3/17/20") as Day2,
+    sum("3/18/20") as Day3,
+    sum("3/19/20") as Day4,
+    sum("3/20/20") as Day5,
+    sum("3/21/20") as Day6,
+    sum("3/22/20") as Day7
+    from covid19_death_csv`;
+    try {
+        const data = await pool.query(sql);
+        return data;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+///////////////////// End getChart /////////////////////
+
+
 module.exports = {
     getAllCountry,
     getAllConfirmed,
     getAllRecovered,
     getAllDeath,
-    getLatLong
+    getLatLong,
+    getAllConfirmedByCountry,
+    getAllRecoveredByCountry,
+    getAllDeathsByCountry,
+    getAllConfirmedByState,
+    getAllRecoveredByState,
+    getAllDeathsByState,
+    getLatLongCountry,
+    getLastUpdateRecovered,
+    getLastUpdateDeaths,
+    getTotalConfirmed,
+    getTotalRecovered,
+    getTotalDeaths,
+    getLastWeekConfirmed,
+    getLastWeekRecovered,
+    getLastWeekDeaths
+
+
+
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const Db = require('../Db/database');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   
-  res.render('index');
+  res.render('index', {
+
+  });
 });
 
 router.get('/map', async function(req,res,next) {
@@ -40,6 +42,29 @@ router.get('/about', async function(req,res,next) {
 
 });
 
+router.get('/graph', async function(req, res, next) {
+  
+  const TotalConfirmed = await Db.getTotalConfirmed();
+  const TotalRecovered = await Db.getTotalRecovered();
+  const TotalDeaths = await Db.getTotalDeaths();
+
+  const lastWeekConfirmed = await Db.getLastWeekConfirmed();
+  const lastWeekRecovered = await Db.getLastWeekRecovered();
+  const lastWeekDeaths = await Db.getLastWeekDeaths();
+
+  const Totals = {
+    TotalConfirmed: TotalConfirmed.rows[0].confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    TotalRecovered: TotalRecovered.rows[0].recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+    TotalDeaths: TotalDeaths.rows[0].deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+  }
+
+  res.render('graph', {
+    Totals: Totals,
+    lastWeekConfirmed: lastWeekConfirmed.rows[0],
+    lastWeekRecovered: lastWeekRecovered.rows[0],
+    lastWeekDeaths: lastWeekDeaths.rows[0],
+  });
+});
 
 
 module.exports = router;
